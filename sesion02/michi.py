7e563c582df9442d16da6b298c66607de0dd8707
ArielQ1 <aquizaya1@umsa.bo>
2025-10-04 14:19:28 -0400

    segunda clase

---
+++ sesion02/michi.py
@@ -0,0 +1,28 @@
+class Gato:
+    pass
+# Instanciar
+pelusa = Gato()
+miauricio = Gato()
+
+# Añadir atributos
+pelusa.color = "negro"
+pelusa.genero = "hembra"
+pelusa.edad = 2
+pelusa.castrado = False
+
+miauricio.color = "naranja"
+miauricio.genero = "macho"
+miauricio.edad = 1
+miauricio.castrado = True
+
+# Mostrar atributos
+print("Pelusa: ",pelusa)
+print(pelusa.color)
+print(pelusa.genero)
+print(pelusa.edad)
+print(pelusa.castrado)
+print("Miauricio: ",miauricio)
+print(miauricio.color)
+print(miauricio.genero)
+print(miauricio.edad)
+print(miauricio.castrado)
